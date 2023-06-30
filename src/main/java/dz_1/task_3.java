85f57d26b260f67e73575a544153b52c2766e992
Павел Кабаков <pavelkabakov@gmail.com>
2023-06-30 23:31:44 +0300

    dz1

--- src/main/java/dz_1/task_3.java
+++ src/main/java/dz_1/task_3.java
@@ -1,7 +1,40 @@
 package dz_1;
 
+// Реализовать простой калькулятор (+ - / *)
+
+import java.util.Scanner;
+
+// Реализовать простой калькулятор (+ - / *)
 public class task_3 {
     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Введите первое число: ");
+         int numb_a = sc.nextInt();
+         System.out.print("Введите второе число: ");
+         int numb_b = sc.nextInt();
+         System.out.println("Введите операцию + - / *: ");
+         char operation;
+         operation = sc.next().charAt(0);
+         int res;
+         switch (operation) {
+             case '+':
+                 res = numb_a + numb_b;
+                 System.out.printf("Результат: %d",res);
+                 break;
+             case '-':
+                 res = numb_a - numb_b;
+                 System.out.printf("Результат: %d",res);
+                 break;
+             case '/':
+                 res = numb_a / numb_b;
+                 System.out.printf("Результат: %d",res);
+                 break;
+             case '*':
+                 res = numb_a * numb_b;
+                 System.out.printf("Результат: %d",res);
+                 break;
 
-    }
+         }
+         sc.close();
+     }
 }
--- src/main/java/dz_1/task_4.java
+++ src/main/java/dz_1/task_4.java
@@ -1,7 +1,27 @@
 package dz_1;
 
+//Задано уравнение вида q + w = e, q, w, e >= 0.
+// Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
+// Требуется восстановить выражение до верного равенства.
+// Предложить хотя бы одно решение или сообщить, что его нет.
+
 public class task_4 {
     public static void main(String[] args) {
+        System.out.println("\n\n Задано выражение: 2? + ?5 = 69");
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (20 + i + j * 10 + 5 == 69) {
+                    count += 1;
+                    System.out.println(count + ") 2" + i + " + " + j + "5" + " = 69");
+                }
+            }
+        }
+        System.out.println("\n");
+        if (count == 0) {
+            System.out.println("Решений нет\n");
+        }
 
     }
+
 }
