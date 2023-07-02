09e5f022b7ebe604ddbf3fad4299ce40f9e07c14
Павел Кабаков <pavelkabakov@gmail.com>
2023-07-02 19:35:01 +0300

    add dz2

--- src/main/java/dz_2/task_1.java
+++ src/main/java/dz_2/task_1.java
@@ -3,8 +3,39 @@
 //Если значение null, то параметр не должен попадать в запрос.
 //Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 package dz_2;
+
 public class task_1 {
     public static void main(String[] args) {
-
+        String list = "\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"";
+        System.out.println("Исходная строка:");
+        System.out.println(list);
+        StringBuilder resultSelect = request(list);
+        System.out.println(resultSelect);
     }
+    public static StringBuilder request(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if(arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+
+        }
+        return result;
+    }
+
 }
--- src/main/java/dz_2/task_2.java
+++ src/main/java/dz_2/task_2.java
@@ -2,9 +2,36 @@
 // результат после каждой итерации
 // запишите в лог-файл.
 package dz_2;
-
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.*;
 public class task_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        int [] array = {2, 5, 1, -7, 3, 64, 0};
+        System.out.println(Arrays.toString(array));
+        int[] newArr = listSort(array);
+        System.out.printf("\n Отсортированный массив: \n");
+//        printArrayInt(newArr);
+        System.out.println(Arrays.toString(newArr));
 
+    }
+    public static int[] listSort(int [] arr) throws IOException{
+        Logger logger = Logger.getLogger(task_2.class.getName());
+        FileHandler fh = new FileHandler("logTask_2.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Sort Array");
+        for(int k = 0; k < arr.length-1; k++) {
+            logger.info("Итерация: " + k + "; " + "Массив: " + Arrays.toString(arr) + "\n");
+            for (int i = 0; i < arr.length - k-1; i++){
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
