d09cf106d8c9f76fabd4b3135f028635acc1e6c6
Павел Кабаков <pavelkabakov@gmail.com>
2023-07-29 19:22:51 +0300

    add Ex009

---
+++ src/main/java/les_10/Ex009/Program.java
@@ -0,0 +1,25 @@
+package les_10.Ex009;
+
+/**
+ * Обобщенные методы
+ */
+
+public class Program {
+    public static void main(String[] args) {
+
+        Printer printer = new Printer();
+        String[] people = {"Tom", "Alice", "Sam", "Kate", "Bob", "Helen"};
+        Integer[] numbers = {23, 4, 5, 2, 13, 456, 4};
+        printer.<String>print(people);
+        printer.<Integer>print(numbers);
+    }
+}
+
+class Printer{
+
+    public <T> void print(T[] items){
+        for(T item: items){
+            System.out.println(item);
+        }
+    }
+}
