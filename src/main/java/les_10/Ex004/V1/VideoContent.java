4d2b053a27c3c275ab23aa11a9b9d3b04cb13375
Павел Кабаков <pavelkabakov@gmail.com>
2023-07-29 17:00:00 +0300

    add les_10

---
+++ src/main/java/les_10/Ex004/V1/VideoContent.java
@@ -0,0 +1,7 @@
+package les_10.Ex004.V1;
+
+public class VideoContent extends Content {
+    public VideoContent(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/les_10/Ex004/V2/VideoContent.java
@@ -0,0 +1,7 @@
+package les_10.Ex004.V2;
+
+public class VideoContent extends Content {
+    public VideoContent(String name) {
+        super(name);
+    }
+}
